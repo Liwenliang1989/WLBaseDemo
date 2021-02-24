7695916e0c6a00d06dc2a7c2c53aee1fa83f2a8d
liwenliang <981851091@qq.com>
2021-02-24 14:56:44 +0800

    initial

---
+++ Example/Pods/Target Support Files/Pods-WLBaseDemo_Tests/Pods-WLBaseDemo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WLBaseDemo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WLBaseDemo_Tests
+@end
---
+++ Example/Pods/Target Support Files/WLBaseDemo/WLBaseDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WLBaseDemo : NSObject
+@end
+@implementation PodsDummy_WLBaseDemo
+@end
